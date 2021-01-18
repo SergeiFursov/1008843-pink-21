17bd6e5426ceb7adc9db2a22d69f445e7fd700f0
Sergey Fursov <fursov_sergei@mail.ru>
2021-01-18 13:58:48 +0500

    Адаптивная графика для страницы каталога

--- source/js/script.js
+++ source/js/script.js
@@ -5,12 +5,7 @@ var navToggle = document.querySelector('.main-navigation__toggle');
 
 navMain.classList.remove('page-header--nojs');
 
-navToggle.addEventListener('click', function() {
-  if(navMain.classList.contains('page-header--closed')) {
-    navMain.classList.remove('page-header--closed');
-    navMain.classList.add('page-header--opened');
-  } else {
-    navMain.classList.add('page-header--closed');
-    navMain.classList.remove('page-header--opened');
-  }
-});
+navToggle.onclick = function() {
+  navMain.classList.toggle('page-header--closed');
+  navMain.classList.toggle('page-header--opened');
+};
